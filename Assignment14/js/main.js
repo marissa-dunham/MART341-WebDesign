2e51c1879b47bc40e51a883ab187a5781f51f7ce
Marissa Dunham <157167966+marissa-dunham@users.noreply.github.com>
2024-05-05 18:20:30 -0700

    Update

---
+++ Assignment14/js/main.js
@@ -0,0 +1,14 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+
+ctx.font = "30px Arial";
+ctx.strokeText("Hello World", 10, 50);
